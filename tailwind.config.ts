21549c26206077ff3c250f30ee485d5edbcf540c
JoshMcDaniel <jrmcdaniel08@yahoo.com>
2024-01-02 00:32:17 -0500

    Add Daisy UI Add theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -15,6 +15,29 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
-}
-export default config
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: [
+      {
+        customTheme: {
+          primary: '#C3E2C2', // light green
+          secondary: '#DBCC95', // dark gold
+          accent: '#CD8D7A', // reddish
+          neutral: '#FBF6EE', // cream
+          'base-100': '#ffffff',
+
+          '--rounded-box': '1rem', // border radius rounded-box utility class, used in card and other large boxes
+          '--rounded-btn': '0.5rem', // border radius rounded-btn utility class, used in buttons and similar element
+          '--rounded-badge': '1.9rem', // border radius rounded-badge utility class, used in badges and similar
+          '--animation-btn': '0.25s', // duration of animation when you click on button
+          '--animation-input': '0.2s', // duration of animation for inputs like checkbox, toggle, radio, etc
+          '--btn-focus-scale': '0.95', // scale transform of button when you focus on it
+          '--border-btn': '1px', // border width of buttons
+          '--tab-border': '1px', // border width of tabs
+          '--tab-radius': '0.5rem', // border radius of tabs
+        },
+      },
+    ],
+  },
+};
+export default config;
