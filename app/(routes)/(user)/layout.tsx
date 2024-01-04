5c7a4ce540e156ec9eb6c375bfc0a42a2c8a4761
JoshMcDaniel <jrmcdaniel08@yahoo.com>
2024-01-03 23:49:02 -0500

    Add new font

--- app/(routes)/(user)/layout.tsx
+++ app/(routes)/(user)/layout.tsx
@@ -1,10 +1,14 @@
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 import '../../globals.css';
+import type { Metadata } from 'next';
 import Navbar from '@/components/Navbar';
 import appConfig from '../../../app-config.json';
+import { Merriweather } from '@next/font/google';
 
-const inter = Inter({ subsets: ['latin'] });
+const merriweather = Merriweather({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-merriweather',
+});
 
 export const metadata: Metadata = {
   title: appConfig.companyName,
@@ -18,8 +22,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="bg-gradient-to-b from-base-100 to-neutral">
-      <body className={inter.className}>
+    <html
+      lang="en"
+      className={
+        `${merriweather.variable} font-sans ` +
+        'bg-gradient-to-b from-base-100 to-neutral'
+      }
+    >
+      <body>
         <Navbar />
         {children}
       </body>
