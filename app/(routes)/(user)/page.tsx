f217fd598b7b731ddcb5af3e9d6f0b3e47b21d59
JoshMcDaniel <jrmcdaniel08@yahoo.com>
2024-01-03 23:01:55 -0500

    Add Home page content init

--- app/(routes)/(user)/page.tsx
+++ app/(routes)/(user)/page.tsx
@@ -1,7 +1,9 @@
-import Image from 'next/image';
+import HomeHero from '@/app/_components/HomeHero';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <HomeHero />
+    </main>
   );
 }
--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -1,6 +1,6 @@
 const Navbar = () => {
   return (
-    <div className="navbar bg-base-100">
+    <div className="navbar fixed top-0 z-50 glass">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
