f8123693721aa7f963f8395399a8e4bf942f1e4e
JoshMcDaniel <jrmcdaniel08@yahoo.com>
2024-01-04 23:47:33 -0500

    Add Footer contact info

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -1,7 +1,8 @@
 import Link from 'next/link';
 import { OrganizationData } from '../_types';
-import { FaFacebookF } from 'react-icons/fa';
 import Image from 'next/image';
+import { FiPhone, FiFacebook } from 'react-icons/fi';
+import { MdOutlineEmail } from 'react-icons/md';
 
 type Props = {
   organization: OrganizationData;
@@ -9,33 +10,63 @@ type Props = {
 
 const Footer = ({ organization }: Props) => {
   return (
-    <footer className="footer p-10 bg-neutral text-neutral-content">
+    <footer className="footer p-8 lg:p-10 bg-neutral text-neutral-content">
       <aside>
-        <Image
-          src={'/images/icon.png'}
-          alt={`${organization.name} company logo.`}
-          priority
-          width={100}
-          height={100}
-        />
+        <div className="w-[104px] h-[104px] rounded-full bg-secondary relative">
+          <Image
+            src={'/images/icon.png'}
+            alt={`${organization.name} company logo.`}
+            priority
+            width={100}
+            height={100}
+            className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+          />
+        </div>
         <p>
           {organization.name}
           <br />
           {organization.slogan}
         </p>
       </aside>
-      <nav>
-        <header className="footer-title">Social</header>
-        <div className="grid grid-flow-col gap-4">
-          <Link
-            href={organization.social.facebook}
-            target="_blank"
-            className="text-3xl"
-          >
-            <FaFacebookF />
-          </Link>
+      <aside className="grid grid-flow-row gap-8">
+        <nav>
+          <header className="footer-title">Social</header>
+          <div className="grid grid-flow-col gap-4">
+            <Link
+              href={organization.social.facebook}
+              target="_blank"
+              className="text-3xl"
+            >
+              <FiFacebook />
+            </Link>
+          </div>
+        </nav>
+        <div>
+          <header className="footer-title">Contact</header>
+          <div className="grid grid-flow-row gap-4">
+            <Link
+              href={`tel:${organization.phoneNumber}`}
+              className="grid grid-flow-col gap-4 items-center justify-start"
+            >
+              <span className="text-2xl w-fit">
+                <FiPhone />
+              </span>
+              <span className="text-xs lg:text-sm">
+                {organization.phoneNumber}
+              </span>
+            </Link>
+            <Link
+              href={`mailto:${organization.email}`}
+              className="grid grid-flow-col gap-4 items-center justify-start"
+            >
+              <span className="text-2xl w-fit">
+                <MdOutlineEmail />
+              </span>
+              <span className="text-xs lg:text-sm">{organization.email}</span>
+            </Link>
+          </div>
         </div>
-      </nav>
+      </aside>
     </footer>
   );
 };
