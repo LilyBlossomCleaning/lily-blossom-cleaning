0388d1a51c8d03f92724bece2d94ed39adfc3040
JoshMcDaniel <jrmcdaniel08@yahoo.com>
2024-03-06 23:57:56 -0500

    Add initial About Us page content

---
+++ app/_components/AboutPage/CompanyStory.tsx
@@ -0,0 +1,46 @@
+type Props = {};
+
+function CompanyStory({}: Props) {
+  return (
+    <div className="prose xl:prose-xl card shadow-md mx-auto">
+      <div className="card-body w-fit m-auto">
+        <h3 className="text-2xl underline decoration-wavy decoration-accent underline-offset-8 lg:underline-offset-[12px] !mb-12">
+          Our Story
+        </h3>
+        <p>
+          Behind our name is a story of passion, dedication, and a deep-rooted
+          belief in the transformative power of a clean, fresh, and invigorating
+          environment.
+        </p>
+        <p>
+          At the heart of Lily Blossom Cleaning is our founder, a visionary with
+          a meticulous eye for detail and a heart full of determination to
+          elevate cleaning services to an art form. With years of personal
+          experience in creating spotless, harmonious spaces, our founder&apos;s
+          journey from a solo enthusiast to the soul of Lily Blossom Cleaning
+          encapsulates a commitment to excellence and a personalized touch that
+          sets our services apart.
+        </p>
+        <p>
+          In our pursuit of creating the perfect cleaning experience, we have
+          honed our methods, carefully selected eco-friendly products, and
+          embraced innovative techniques that ensure a superior clean without
+          compromising the well-being of our clients or the planet. Our holistic
+          approach goes beyond the physical aspects of cleaning; we aim to
+          create a sense of peace and serenity, turning every space we touch
+          into a haven of cleanliness and tranquility.
+        </p>
+        <p>
+          Lily Blossom Cleaning is more than just a company; it&apos;s a promise
+          of unwavering quality and unmatched dedication to making your world a
+          cleaner, brighter place. We invite you to be part of our story, to
+          experience the difference that comes with a Lily Blossom clean, and to
+          join us as we continue to spread beauty and cleanliness one space at a
+          time.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default CompanyStory;
