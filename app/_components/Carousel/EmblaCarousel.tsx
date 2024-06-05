828f7d04ac08df6c1dac46f54fd8d5258d9eb329
JoshMcDaniel <jrmcdaniel08@yahoo.com>
2024-06-04 23:27:24 -0400

    Change to fade carousel

--- app/_components/Carousel/EmblaCarousel.tsx
+++ app/_components/Carousel/EmblaCarousel.tsx
@@ -1,22 +1,17 @@
 'use client';
 
-import React, { useCallback, useEffect, useRef, useState } from 'react';
-import {
-  EmblaCarouselType,
-  EmblaEventType,
-  EmblaOptionsType,
-} from 'embla-carousel';
+import sanityUrlFor from '@/sanity/lib/sanityUrlFor';
+import BlockContent from '@sanity/block-content-to-react';
+import { EmblaOptionsType } from 'embla-carousel';
 import useEmblaCarousel from 'embla-carousel-react';
+import Fade from 'embla-carousel-fade';
 import {
   NextButton,
   PrevButton,
   usePrevNextButtons,
 } from './EmblaCarouselArrowButtons';
-import sanityUrlFor from '@/sanity/lib/sanityUrlFor';
+// import { DotButton, useDotButton } from './EmblaCarouselDotButton';
 import { ServiceDetails } from '@/app/@types/_types';
-import BlockContent from '@sanity/block-content-to-react';
-
-const TWEEN_FACTOR_BASE = 0.2;
 
 type PropType = {
   slides: number[];
@@ -26,9 +21,10 @@ type PropType = {
 
 const EmblaCarousel: React.FC<PropType> = (props) => {
   const { slides, options, services } = props;
-  const [emblaRef, emblaApi] = useEmblaCarousel(options);
-  const tweenFactor = useRef(0);
-  const tweenNodes = useRef<HTMLElement[]>([]);
+  const [emblaRef, emblaApi] = useEmblaCarousel(options, [Fade()]);
+
+  // const { selectedIndex, scrollSnaps, onDotButtonClick } =
+  //   useDotButton(emblaApi);
 
   const {
     prevBtnDisabled,
@@ -37,128 +33,56 @@ const EmblaCarousel: React.FC<PropType> = (props) => {
     onNextButtonClick,
   } = usePrevNextButtons(emblaApi);
 
-  const setTweenNodes = useCallback((emblaApi: EmblaCarouselType): void => {
-    tweenNodes.current = emblaApi.slideNodes().map((slideNode) => {
-      return slideNode.querySelector('.embla__parallax__layer') as HTMLElement;
-    });
-  }, []);
-
-  const setTweenFactor = useCallback((emblaApi: EmblaCarouselType) => {
-    tweenFactor.current = TWEEN_FACTOR_BASE * emblaApi.scrollSnapList().length;
-  }, []);
-
-  const tweenParallax = useCallback(
-    (emblaApi: EmblaCarouselType, eventName?: EmblaEventType) => {
-      const engine = emblaApi.internalEngine();
-      const scrollProgress = emblaApi.scrollProgress();
-      const slidesInView = emblaApi.slidesInView();
-      const isScrollEvent = eventName === 'scroll';
-
-      emblaApi.scrollSnapList().forEach((scrollSnap, snapIndex) => {
-        let diffToTarget = scrollSnap - scrollProgress;
-        const slidesInSnap = engine.slideRegistry[snapIndex];
-
-        slidesInSnap.forEach((slideIndex) => {
-          if (isScrollEvent && !slidesInView.includes(slideIndex)) return;
-
-          if (engine.options.loop) {
-            engine.slideLooper.loopPoints.forEach((loopItem) => {
-              const target = loopItem.target();
-
-              if (slideIndex === loopItem.index && target !== 0) {
-                const sign = Math.sign(target);
-
-                if (sign === -1) {
-                  diffToTarget = scrollSnap - (1 + scrollProgress);
-                }
-                if (sign === 1) {
-                  diffToTarget = scrollSnap + (1 - scrollProgress);
-                }
-              }
-            });
-          }
-
-          const translate = diffToTarget * (-1 * tweenFactor.current) * 100;
-          const tweenNode = tweenNodes.current[slideIndex];
-          tweenNode.style.transform = `translateX(${translate}%)`;
-        });
-      });
-    },
-    []
-  );
-
-  const [index, setSelectedIndex] = useState(0);
-  const onSelect = useCallback(() => {
-    if (!emblaApi) return;
-    setSelectedIndex(emblaApi.selectedScrollSnap());
-  }, [emblaApi]);
-
-  useEffect(() => {
-    if (!emblaApi) return;
-    emblaApi.on('select', onSelect);
-  }, [emblaApi, onSelect]);
-
-  useEffect(() => {
-    if (!emblaApi) return;
-
-    setSelectedIndex(emblaApi.selectedScrollSnap());
-
-    setTweenNodes(emblaApi);
-    setTweenFactor(emblaApi);
-    tweenParallax(emblaApi);
-
-    emblaApi
-      .on('reInit', setTweenNodes)
-      .on('reInit', setTweenFactor)
-      .on('reInit', tweenParallax)
-      .on('scroll', tweenParallax)
-      .on('slideFocus', tweenParallax);
-  }, [emblaApi, tweenParallax, onSelect]);
-
   return (
-    <div>
-      <div className="embla">
-        <div className="embla__viewport" ref={emblaRef}>
-          <div className="embla__container">
-            {slides.map((index) => (
-              <div className="embla__slide relative" key={index}>
-                <div className="embla__parallax">
-                  <div className="embla__parallax__layer">
-                    <img
-                      className="embla__slide__img embla__parallax__img"
-                      src={`${sanityUrlFor(services[index].image).url()}`}
-                      alt={services[index].name}
+    <div className="embla relative">
+      <div
+        className="embla__viewport shadow-2xl rounded-2xl relative"
+        ref={emblaRef}
+      >
+        <div className="embla__container">
+          {slides.map((index) => (
+            <div className="embla__slide" key={index}>
+              <span className="relative">
+                <div className="embla__buttons z-10 px-4">
+                  <span className="glass rounded-full w-fit">
+                    <PrevButton
+                      onClick={onPrevButtonClick}
+                      disabled={prevBtnDisabled}
                     />
-                    <span className="absolute bottom-0 left-0 right-0 glass px-4 py-2 card-title grid justify-items-center">
-                      {services[index].name}
-                    </span>
-                  </div>
+                  </span>
+                  <span className="glass rounded-full w-fit">
+                    <NextButton
+                      onClick={onNextButtonClick}
+                      disabled={nextBtnDisabled}
+                    />
+                  </span>
                 </div>
+                <img
+                  className="embla__slide__img"
+                  src={`${sanityUrlFor(services[index].image).url()}`}
+                  alt={services[index].name}
+                />
+                <span className="absolute inset-0 bg-primary bg-opacity-60 px-20 py-2 grid content-center text-center font-allura text-5xl lg:text-7xl">
+                  {services[index].name}
+                </span>
+              </span>
+              <div className="prose xl:prose-xl mx-auto p-8">
+                <BlockContent blocks={services[index].description} />
               </div>
-            ))}
-          </div>
+            </div>
+          ))}
         </div>
-
-        <div className="embla__controls">
-          <div className="embla__buttons px-4">
-            <span className="glass rounded-full w-fit">
-              <PrevButton
-                onClick={onPrevButtonClick}
-                disabled={prevBtnDisabled}
-              />
-            </span>
-            <span className="glass rounded-full w-fit">
-              <NextButton
-                onClick={onNextButtonClick}
-                disabled={nextBtnDisabled}
-              />
-            </span>
-          </div>
-        </div>
-      </div>
-
-      <div className="prose xl:prose-xl mx-auto p-8">
-        <BlockContent blocks={services[index].description} />
+        {/* <div className="embla__dots absolute bottom-0 right-0 py-2 px-4">
+          {scrollSnaps.map((_, index) => (
+            <DotButton
+              key={index}
+              onClick={() => onDotButtonClick(index)}
+              className={'embla__dot'.concat(
+                index === selectedIndex ? ' embla__dot--selected' : ''
+              )}
+            />
+          ))}
+        </div> */}
       </div>
     </div>
   );
